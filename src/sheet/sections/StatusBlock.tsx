ca4eb07847ac4da7704d079fe6212c84f5a42a84
Patrick Cassel <patrick.cassel@googlemail.com>
2023-05-25 21:32:11 +0200

    set damage values

--- src/sheet/sections/StatusBlock.tsx
+++ src/sheet/sections/StatusBlock.tsx
@@ -5,9 +5,11 @@ import CharacterProps from "../CharacterProps";
 
 export default function StatusBlock({character, characterDispatch}: CharacterProps): React.ReactElement {
 
-    function DamageSelect({level}: { level: DamageLevel }): React.ReactElement {
-        return <select className={"label"} value={level} onChange={event => {
-        }}>
+    function DamageSelect({level, changeFunction}: {
+        level: DamageLevel,
+        changeFunction: (value: string) => void
+    }): React.ReactElement {
+        return <select className={"label"} value={level} onChange={event => changeFunction(event.target.value)}>
             <option value={DamageLevel.None}></option>
             <option value={DamageLevel.Bashing}>/</option>
             <option value={DamageLevel.Lethal}>x</option>
@@ -21,31 +23,60 @@ export default function StatusBlock({character, characterDispatch}: CharacterPro
             <h3 className="property-block-heading">Health</h3>
             <div className="labeled-entry">
                 <span className="label">Bruised</span>
-                <DamageSelect level={character.bruised}/>
+                <DamageSelect level={character.bruised} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "bruised",
+                    value: value
+                })}
+                />
             </div>
             <div className="labeled-entry">
                 <span className="label">Hurt (-1)</span>
-                <DamageSelect level={character.hurt}/>
+                <DamageSelect level={character.hurt} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "hurt",
+                    value: value
+                })}/>
             </div>
             <div className="labeled-entry">
                 <span className="label">Injured (-1)</span>
-                <DamageSelect level={character.injured}/>
+                <DamageSelect level={character.injured} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "injured",
+                    value: value
+                })}/>
             </div>
             <div className="labeled-entry">
                 <span className="label">Wounded (-2)</span>
-                <DamageSelect level={character.wounded}/>
+                <DamageSelect level={character.wounded} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "wounded",
+                    value: value
+                })}/>
             </div>
             <div className="labeled-entry">
                 <span className="label">Mauled (-2)</span>
-                <DamageSelect level={character.mauled}/>
+                <DamageSelect level={character.mauled} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "mauled",
+                    value: value
+                })}/>
             </div>
             <div className="labeled-entry">
                 <span className="label">Crippled (-5)</span>
-                <DamageSelect level={character.crippled}/>
+                <DamageSelect level={character.crippled} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "crippled",
+                    value: value
+                })}/>
             </div>
             <div className="labeled-entry">
                 <span className="label">Incapacitated</span>
-                <DamageSelect level={character.incapacitated}/>
+                <DamageSelect level={character.incapacitated} changeFunction={value => characterDispatch({
+                    type: "setProperty",
+                    property: "incapacitated",
+                    value: value
+                })}/>
             </div>
         </div>
         <div>
