a062b764a0ed2ca041328c1a4c80925df6779ecf
Patrick Cassel <patrick.cassel@googlemail.com>
2023-06-16 22:27:36 +0200

    refactor StatSearchItem

--- src/topbar/SearchItems.ts
+++ src/topbar/SearchItems.ts
@@ -1,4 +1,11 @@
 import {capitalizeFirstLetter} from "../Helper";
+import Character from "../model/Character";
+
+type stat = keyof Character
+
+export enum SearchItemType {
+    STAT = "stat",
+}
 
 export interface SearchItem {
     type: SearchItemType;
@@ -6,60 +13,70 @@ export interface SearchItem {
     name: string;
 }
 
-export enum SearchItemType {
-    STAT = "stat",
-}
+export class StatSearchItem implements SearchItem {
+    stat: stat;
+    name: string;
+
+    type: SearchItemType = SearchItemType.STAT;
+
+    get value(): string {
+        return this.stat
+    };
 
 
-function statEntry(value: string): SearchItem {
-    return {
-        type: SearchItemType.STAT,
-        value: value,
-        name: capitalizeFirstLetter(value)
+    constructor(stat: stat, name: string) {
+        this.stat = stat;
+        this.name = name;
     }
 }
 
-export const searchItems: SearchItem[] = [
-    ...["strength",
-        "dexterity",
-        "stamina",
-        "charisma",
-        "manipulation",
-        "appearance",
-        "perception",
-        "intelligence",
-        "wits",
-        "alertness",
-        "athletics",
-        "brawl",
-        "dodge",
-        "empathy",
-        "expression",
-        "intimidation",
-        "leadership",
-        "streetwise",
-        "subterfuge",
-        "animalKen",
-        "crafts",
-        "drive",
-        "etiquette",
-        "firearms",
-        "melee",
-        "performance",
-        "security",
-        "stealth",
-        "survival",
-        "academics",
-        "computer",
-        "finance",
-        "investigation",
-        "law",
-        "linguistics",
-        "medicine",
-        "occult",
-        "politics",
-        "science",
-        "conscience",
-        "selfControl",
-        "courage"].map(statEntry),
+function statEntry(stat: stat): SearchItem {
+    return new StatSearchItem(stat, capitalizeFirstLetter(stat));
+}
+
+const stats: stat[] = [
+    "strength",
+    "dexterity",
+    "stamina",
+    "charisma",
+    "manipulation",
+    "appearance",
+    "perception",
+    "intelligence",
+    "wits",
+    "alertness",
+    "athletics",
+    "brawl",
+    "dodge",
+    "empathy",
+    "expression",
+    "intimidation",
+    "leadership",
+    "streetwise",
+    "subterfuge",
+    "animalKen",
+    "crafts",
+    "drive",
+    "etiquette",
+    "firearms",
+    "melee",
+    "performance",
+    "security",
+    "stealth",
+    "survival",
+    "academics",
+    "computer",
+    "finance",
+    "investigation",
+    "law",
+    "linguistics",
+    "medicine",
+    "occult",
+    "politics",
+    "science",
+    "conscience",
+    "selfControl",
+    "courage"
 ]
+
+export const searchItems: SearchItem[] = stats.map(statEntry);
